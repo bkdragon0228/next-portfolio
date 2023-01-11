37b193dce6ddfefaf3842177e607072379c3f97f
bkdragon0228 <rlaqjarb2067@naver.com>
2023-01-11 11:30:38 +0900

    feat : add animation with lottie

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import Layout from "../components/layout";
+import Hero from "../components/home/hero";
+import Animation from "../components/home/animation";
 
 export default function Home() {
   return (
@@ -13,28 +15,7 @@ export default function Home() {
 
       <section className="flex min-h-screen flex-col items-center justify-center text-gray-600 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              안녕하세요.
-              <br className="hidden lg:inline-block" />
-              Next tutorial 입니다.
-            </h1>
-            <p className="mb-8 leading-relaxed">
-              귀는 뛰노는 풀이 붙잡아 몸이 약동하다. 보내는 심장의 든 원질이
-              어디 그것은 청춘 대고, 피가 말이다. 찾아 풀이 과실이 인생에 그들의
-              살았으며, 사랑의 약동하다. 바이며, 꽃이 자신과 사막이다. 희망의
-              이상이 아름답고 트고, 속에서 이상, 우리의 어디 있는가? 청춘 같지
-              원질이 그들은 것이다. 구하지 노래하며 청춘의 듣기만 없는 청춘
-              힘차게 바로 피다. 우리의 같이, 있는 운다. 수 발휘하기 피가 주며,
-              설산에서 교향악이다. 구하지 그들은 산야에 아니다.
-            </p>
-            <div className="flex justify-center">
-              <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-                프로젝트 보러가기
-              </button>
-            </div>
-          </div>
-          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6"></div>
+          <Hero />
         </div>
       </section>
     </Layout>
